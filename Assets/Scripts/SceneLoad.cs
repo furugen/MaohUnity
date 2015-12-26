f60798c0f7048eb2202a2b43f0c0f7b90d986b64
furugen <furugen098@gmail.com>
2015-12-26 22:07:52 +0900

    ページ遷移実装

---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoad : MonoBehaviour {
+
+    public void loadScene(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
